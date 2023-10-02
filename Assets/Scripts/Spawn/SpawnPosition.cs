5e7e8e8decc1dad56c198c7ce429472fba1b20ea
plumas90 <plumas90@naver.com>
2023-10-02 20:38:21 +0900

    맵 수정 스포너 위치지정

---
+++ Assets/Scripts/Spawn/SpawnPosition.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPosition : MonoBehaviour
+{
+
+    public Vector3 ReturnPosition() 
+    {
+        return this.transform.position;
+    }
+}
