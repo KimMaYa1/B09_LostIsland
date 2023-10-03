1eb4eb43eff513502d6f83614dde957142c3fb01
plumas90 <plumas90@naver.com>
2023-10-03 11:23:13 +0900

    아이템 드랍 수정
    공격방식과 유사하나 수정이 필요하다고 생각됨

--- Assets/Scripts/Spawn/Resource.cs
+++ Assets/Scripts/Spawn/Resource.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
     public Item[] dropItem;
-    public int quantityPerHit = 1;
-    public int capacity;
+    //public int quantityPerHit = 1;
+    public int capacity=2;
     public GameObject spawnOBJ;
     private Spawner spawn;
     private void Start()
@@ -18,23 +19,15 @@ public class Resource : MonoBehaviour
     //public void Gather(Vector3 hitPoint, Vector3 hitNormal) 
     public void Gather() 
     {
-        
-        for (int i = 0; i < quantityPerHit; ++i) 
+        capacity -= 1;
+        if (capacity <= 0) 
         {
-            Debug.Log("1차 포문");
-            if (capacity <= 0) { break; }
-            capacity -= 1;
-            for (int j = 0; j < capacity; ++j) 
+            for (int j = 0; j < dropItem.Length; ++j)
             {
                 //Instantiate(dropItem[j].itemPrefab, hitPoint + Vector3.up, Quaternion.LookRotation(hitNormal, Vector3.up));
                 Debug.Log("2차 포문");
                 Instantiate(dropItem[j].itemPrefab, this.transform.position + Vector3.up, Quaternion.identity);
             }
-
-        }
-        if (capacity <= 0) 
-        {
-
             spawn.InsertQueue(gameObject);
             this.gameObject.SetActive(false);
 
@@ -42,7 +35,7 @@ public class Resource : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Weapon" && other.tag == "player")
+        if (other.tag == "Weapon" || other.tag == "player")
         {
             Debug.Log("접촉 테스트 성공");
             Gather();
